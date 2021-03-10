6b3854e9b0869744ddcf1441b4d6271c1ad14ea9
Peerapon Wechsuwanmanee <peerapon.w@gmail.com>
2021-03-10 16:49:41 +0100

    add clean_files function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from configparser import ConfigParser
 from airtable import Airtable
-import sys, os
+import sys, os, glob
 import json
 from jinja2 import Environment, FileSystemLoader
 
@@ -20,12 +20,24 @@ def fetch_airtable(sim_name):
 
     with open(sim_name+'.json', 'w') as f:
         json.dump(res[0]['fields'], f)
+def clean_files(pattern):
+    for f in glob.glob(pattern):
+        os.remove(f)
 
 for sim_name in sim_list:
     # get json file from airtable
     fetch_airtable(sim_name)
-    file_loader = FileSystemLoader("templates")
-    env = Environment(loader=file_loader)
+    # load file from folder templates
+    env = Environment(loader=FileSystemLoader("templates"))
     abq_template = env.get_template('abq.py')
+    # render template
     abq_template.stream(sim_name=sim_name).dump(f"{sim_name}.py")
     os.system(f"abaqus cae noGUI={sim_name}.py")
+    
+    
+    
+    
+    # clean the directory
+    clean_files("abaqus*") # delete all temp files by ABAQUS
+    clean_files(f"{sim_name}*") # delete all intermediate files by this script
+    
